cdce4909215c023b99720aacb6beedd18c4ed83d
cyf <867685217@qq.com>
2017-11-08 11:52:15 +0800

    scrollview莫名白条

--- app/pages/HomePage.js
+++ app/pages/HomePage.js
@@ -220,7 +220,6 @@ export default class HomePage extends Component {
                     hidden={false}
                     animated={false}
                     barStyle={'light-content'}
-                    backgroundColor={'blue'}
                     translucent={true}
                 />
             <ScrollView
